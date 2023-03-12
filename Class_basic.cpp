d58a8e3148d4a7fb160ec75916256d3f5fae2593
Pranav Giri <114610311+PrnvGiri@users.noreply.github.com>
2023-03-12 13:12:33 +0530

    Create Class_basic.cpp
    Here we are just taking an input of name and marks and show the output by using basic Class which is a property of OOP(Object oriented language).

---
+++ Class_basic.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class BCA
+{
+   string name;
+   int marks;
+   
+   public:
+   void getdata()
+   {
+     cout<<"Enter Your name"<<endl;
+     cin>>name;
+     cout<<"Enter your marks"<<endl;
+     cin>>marks;
+   }
+
+   void display()
+   {
+      cout<<"Hello "<<name<<" your marks is "<<marks<<endl;
+   }
+};
+int main()
+{
+   BCA a,b;
+   a.getdata();
+   b.getdata();
+   a.display(),b.display();
+   
+   return 0;
+}
