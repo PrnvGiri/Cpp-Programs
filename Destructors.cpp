19d0cec40504950bae7af61dcbb58de40296480b
Pranav Giri <114610311+PrnvGiri@users.noreply.github.com>
2023-04-17 22:39:20 +0530

    Create Destructors.cpp

---
+++ Destructors.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class sum
+{
+    int a,b;
+    public:
+    sum()
+    {
+       cout<<"Enter Your Numbers"<<endl;
+       cin>>a>>b;
+    }
+    sum(int x, int y)
+    {
+        cout<<"Enter Your Numbers using parameters"<<endl;
+       cin>>a>>b;
+    }
+    sum(sum &u)
+    {
+        cout<<"Enter Your Numbers using parameters"<<endl;
+       cin>>a>>b;
+    }
+    void display()
+    {
+      int c = a+b;
+        cout<<"The sum for the number "<<a<<" and "<<b<<" is "<<c<<endl;
+    }    
+    ~sum()
+    {
+        cout<<"Destructor called"<<endl;
+    }
+};
+
+int main()
+{
+    int a,b;
+    sum ob,ob1(a,b);
+    ob.display();
+    ob1.display();
+}
